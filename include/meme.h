3aeb65615bbc2b6262db180df107455e237d6872
Brian Lindsay <blindsay@iastate.edu>
2017-06-16 20:50:04 -0500

    added more meme defines

--- include/meme.h
+++ include/meme.h
@@ -1,33 +1,45 @@
 #include <ncurses.h>
 #include "ui/page.h"
 #include "ui/ui.h"
-#define your GUMIN 6 TUTURU "                                              _       __/ |"NYANPASU GUMIN 7 TUTURU "                                             | |     |___/       "NYANPASU
+#define your GUMIN 6 TUTURU DRILL SHOKUGEKI "       _       " C "|"PIERCE 7 TUTURU DRILL SHOKUGEKI "      | |     |" WAIFU "/       "NYANPASU
 #define SEE BONE OF
 #define YOU KUN TRUTH
-#define CONTRADICTION void ExitPage::draw()
+#define CONTRADICTION void TRAP ::draw HEAVENS 
 #define POMF SUCH MISFORTUNE NANI
-#define OF ExitPage(UI
-#define way  GUMIN 8 TUTURU " ___ _ __   __ _  ___ ___    ___ _____      _| |__   ___  _   _       "NYANPASU GUMIN 9 TUTURU "/ __| '_ \\ / _` |/ __/ _ \\  / __/ _ \\ \\ /\\ / / '_ \\ / _ \\| | | |      "NYANPASU GUMIN 10 TUTURU "\\__ \\ |_) | (_| | (_|  __/ | (_| (_) \\ V  V /| |_) | (_) | |_| |_ _ _ "NYANPASU
+#define C "__/ "
+#define OF TRAP (UI
+#define way  GUMIN 8 TUTURU " " WAIFU " _ __   __ _  " THE "    " THE "__      _| |__   " WAIFU "  _   _       "PIERCE 9 TUTURU "/ __| '_ " T " / _` |/ " C "_ " T "  / " C "_ " THAT " /" T " / / '_ " T " / _ " T "| | | |      "PIERCE 10 TUTURU  T "__ " T " " ANIME " " T " V  V /| |_) | (_) | |_| |_ _ _ "NYANPASU
 #define MY *ui):BlankPage
 #define NYANPASU );
 #define SWORD (ui){}
-#define IS {clear();int i,
+#define IS {clear HEAVENS ;int i,
 #define TRUTH j,j2;i=9;
-#define MISFORTUNE BAKU HATSU 0,j,"             _____"); BAKU HATSU 1,j,"            /  ___|");
+#define ANIME "|_) | " A " | " A "  " C "| " A " (_)"
+#define MISFORTUNE BAKU HATSU 0,j,SHOKUGEKI WAIFU "__"); BAKU HATSU 1,j,"            /  " WAIFU "|");
 #define BAKU mvprintw
-#define NAISU }void ExitPage::input(){getch();parent->close();}
-#define DENWA GUMIN 13 TUTURU "    |_|                                                   |___/       "NYANPASU NAISU
-#define GELNANA "|___/ .__/ \\__,_|\\___\\___|  \\___\\___/ \\_/\\_/ |_.__/ \\___/ \\__, (_|_|_)"
-#define dont GUMIN 2 BANG"            \\ `--.   ___   ___   _   _   __ _"NYANPASU GUMIN 3 BANG "             `--. \\ / _ \\ / _ \\ | | | | / _` |"NYANPASU
+#define NAISU }void TRAP ::input HEAVENS {getch HEAVENS ;parent->close HEAVENS ;}
+#define DENWA GUMIN 13 TUTURU "    |_|" DRILL SHOKUGEKI "            |" WAIFU "/       "NYANPASU NAISU
+#define GELNANA "|" WAIFU "/ ." C  T "__,_|" T WAIFU  T WAIFU "|  " T  WAIFU  T WAIFU "/ " T "_/" T "_/ |_." C  T  WAIFU "/ " T "__, " A "_|_)"
+#define dont GUMIN 2 BANG"            " T " `--.   " WAIFU "   " WAIFU "   _   _   __ _"PIERCE 3 BANG SHOKUGEKI "`--. " T " / _ " T " / _ " T " | | | | / _` |"NYANPASU
 #define HATSU (i+
-#define KYON GUMIN 11 TUTURU GELNANA NYANPASU GUMIN 12 TUTURU "    | |                                                    __/ |      "NYANPASU
+#define TRAP ExitPage
+#define A "(_|"
+#define THE WAIFU " " WAIFU
+#define HEAVENS ()
+#define KYON GUMIN 11 TUTURU GELNANA PIERCE 12 TUTURU "    | |" DRILL DRILL  C "|      "NYANPASU
 #define COWBOY DENWA
 #define SUCH j=29;j2=9;
 #define SPACE POMF
-#define loss GUMIN 4 BANG "            /\\__/ /|  __/|  __/ | |_| || (_| |"NYANPASU GUMIN 5 BANG "            \\____/  \\___| \\___|  \\__, | \\__,_|"NYANPASU
+#define loss GUMIN 4 BANG "            /" T  C "/|  __/|  " C "| |_| || " A " |"PIERCE 5 BANG "            " T  WAIFU "_/  " T  WAIFU "| " T  WAIFU "|  " T "__, | " T "__,_|"NYANPASU
 #define KUN MY SWORD CONTRADICTION IS
 #define GUMIN BAKU HATSU 
 #define BANG ,j,
-#define BONE ExitPage:: 
+#define BONE TRAP :: 
 #define TUTURU ,j2,
+#define SHOKUGEKI "             "
 #define NANI dont loss your way KYON
+#define WAIFU "___"
+#define DRILL SHOKUGEKI SHOKUGEKI
+#define T "\\"
+#define THAT T " " T
+#define PIERCE NYANPASU GUMIN
